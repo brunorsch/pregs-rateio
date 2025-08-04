43a36064643aa587b5c60c3830a5e18b81878b3b
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2025-08-03 21:10:16 -0300

    refactor: Adicionando novo server em Spring

---
+++ server/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "br.app.pregsrateio"
+version = "1.0.0"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+object Versions {
+	const val springdoc = "2.8.9"
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.micrometer:micrometer-tracing-bridge-brave")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${Versions.springdoc}")
+
+
+	compileOnly("org.projectlombok:lombok")
+
+	annotationProcessor("org.projectlombok:lombok")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ server/src/main/java/br/app/pregsrateio/PregsrateioApplication.java
@@ -0,0 +1,13 @@
+package br.app.pregsrateio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PregsrateioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PregsrateioApplication.class, args);
+	}
+
+}
