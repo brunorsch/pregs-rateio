d735f70cb903615248a37c16ccb0813fe9034751
Bruno Ribeiro Schmidt <brunoribeiro.sch@gmail.com>
2025-08-08 23:52:18 -0300

    feat: Criando entidades de historico e evento de auditoria

---
+++ server/src/main/java/br/app/pregsrateio/rateio/CrudRateioService.java
@@ -0,0 +1,11 @@
+package br.app.pregsrateio.rateio;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CrudRateioService {
+
+}
